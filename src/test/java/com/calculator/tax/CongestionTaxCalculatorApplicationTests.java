911190d31df588c5e8044f2112f695d0ca977595
Max Gomes <maxgomes92@live.com>
2023-01-25 12:05:38 +0100

    Init spring boot application

---
+++ src/test/java/com/calculator/tax/CongestionTaxCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.tax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongestionTaxCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
